66a6abe9246f353bfa32ae110776767b957fc074
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-11-26 13:00:36 -0800

    add path to index html

---
+++ main.tf
@@ -0,0 +1,202 @@
+data "aws_caller_identity" "current" {}
+
+
+resource "aws_s3_bucket" "website" {
+  bucket = "projectfall.click"   
+
+  tags = {
+    Name = "projectfall-website"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "website" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website,
+    aws_s3_bucket_public_access_block.website,
+  ]
+
+  bucket = aws_s3_bucket.website.id
+  acl    = "private"
+}
+
+############################
+
+
+data "aws_route53_zone" "main" {
+  name         = "projectfall.click."
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "site_cert" {
+  provider = aws.us-east-1
+
+  domain_name               = "projectfall.click"
+  subject_alternative_names = ["www.projectfall.click"]
+
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "site_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.site_cert.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "site_cert_validation" {
+  provider = aws.us-east-1
+
+  certificate_arn         = aws_acm_certificate.site_cert.arn
+  validation_record_fqdns = [for r in aws_route53_record.site_cert_validation : r.fqdn]
+}
+
+
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "projectfall-s3-oac"
+  description                       = "OAC for private S3 origin"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "website" {
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "Projectfall static site via CloudFront"
+  aliases         = ["projectfall.click", "www.projectfall.click"]
+
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "s3-origin-${aws_s3_bucket.website.id}"
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+  }
+
+  default_root_object = "site/index.html"
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin-${aws_s3_bucket.website.id}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.site_cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  depends_on = [
+    aws_acm_certificate_validation.site_cert_validation
+  ]
+}
+
+data "aws_iam_policy_document" "website_bucket_policy" {
+  statement {
+    sid    = "AllowCloudFrontRead"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.website.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.website.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.website_bucket_policy.json
+}
+
+resource "aws_route53_record" "root_a" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "projectfall.click"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www_a" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "www.projectfall.click"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
