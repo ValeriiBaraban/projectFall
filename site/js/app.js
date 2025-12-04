4338c9148413fb55c906192b8aa9cce1fdf7e038
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-03 16:53:10 -0800

    change wheater provider

--- site/js/app.js
+++ site/js/app.js
@@ -180,15 +180,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+////////////////////////////////////////////
+TODO//fetch geolocation by city name.
+//https://api.open-meteo.com/v1/forecast?latitude=51.5072&longitude=-0.1276&current_weather=true
 
 
-const BASEURL = 'https://api.weatherapi.com/v1';
+const BASEURL = 'https://api.open-meteo.com/v1';
 
 //const BASEURL = 'api.weatherapi.com/v1'
-const typeData = 'current.json';
-const query = 'London';
-const aqi = 'no';
-const url = `http://${BASEURL}/${typeData}?key=${API_KEY}&q=${query}&aqi=${aqi}`;
+const typeData = 'forecast';
+const latitude =  51.507;
+const longitude = -0.1276;
+const current_weather = true;
+const url = `http://${BASEURL}/${typeData}?latitude=${latitude}&longitude=${longitude}&current_weather=${current_weather}`;
 //
 
 
