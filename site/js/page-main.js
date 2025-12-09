398da63d7b34369f3013c8a7adbf0cbfa5dc8223
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-08 23:52:10 -0800

    test getFile

--- site/js/page-main.js
+++ site/js/page-main.js
@@ -194,8 +194,10 @@ const getZipCity = async () => {
 const getFile = async () => {
   await getZipCity();
   const data = JSON.parse(sessionStorage.getItem('zipCity'));
-  const getCity = data.filter((item) => item.physical_city.toLowerCase() === 'seattle')
+  const getZip = data.filter((item) => item.physical_city.toLowerCase() === 'seattle');
+  const getCity = data.filter((item) => item.delivery_zipcode === 98155);
 
+  console.log(getZip);
   console.log(getCity);
 }
 getFile();
