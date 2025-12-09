8e353f8451eb28216830d07d58f9d8c03c5c0f4f
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-08 20:12:48 -0800

    test sortingJson

--- site/js/page-main.js
+++ site/js/page-main.js
@@ -191,6 +191,10 @@ const sortingJson = async () => {
   const res = await fetch('/site/delivery_zipcode_physical_city.json');
 
   const file = await res.json();
+  sessionStorage.setItem('keys', JSON.stringify(file));
   console.log(file);
+
+  return file;
 }
 sortingJson();
+
