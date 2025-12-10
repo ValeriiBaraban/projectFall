dae98470b280a89af41d44ef87c23a0ff1b436a3
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-09 16:53:49 -0800

    refactor app.js

--- site/js/app.js
+++ site/js/app.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     // turn off browser validation
     document.querySelector('form').setAttribute('novalidate', '');
     
-    //errors for field: City
+    // Get the error message container
+    const error = document.querySelector('.alert-text');
+ 
+    //errors for field: Name
     const cityError = document.querySelector('#city-error');
     const cityField = document.querySelector('#cities');
-    
+    // nameField.setAttribute('required', '');
+    // nameField.setAttribute('minlength', 3);
+
     // checking City
     const checkingFieldName = () => {
         if(cityField.value.length === 0) {
