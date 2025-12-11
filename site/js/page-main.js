4f38eccedd0cfc7c439955450515b7c5e4c3ac1f
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-11 11:40:58 -0800

    fix call variable

--- site/js/page-main.js
+++ site/js/page-main.js
@@ -231,11 +231,12 @@ const getZipCity = async () => {
 const searchInFile = async () => {
   await getZipCity();
   const data = JSON.parse(sessionStorage.getItem('zipCity'));
-  const getZip = data.filter((item) => item.physical_city.toLowerCase() === sessionStorage.getItem(normalizeCityName()).toLowerCase());
+  const getZip = data.filter((item) => item.physical_city.toLowerCase() === sessionStorage.getItem('selectedCity').toLowerCase());
   //const getCity = data.filter((item) => item.delivery_zipcode === sessionStorage.getItem('zip'));
 
   console.log('zip', getZip);
 }
-console.log(searchInFile());
+searchInFile();
 
 });
+searchInFile();
