1546f0921e43e64f34b52a4b457720802a469a18
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-11 13:11:21 -0800

    test searchFunction

--- site/js/page-main.js
+++ site/js/page-main.js
@@ -230,8 +230,8 @@ const getZipCity = async () => {
 
 const searchInFile = async () => {
   await getZipCity();
-  const data = JSON.parse(sessionStorage.getItem('zipCity'));
-  const getZip = data.filter((item) => item.physical_city.toLowerCase()); // === sessionStorage.getItem('selectedCity').toLowerCase());
+  const data = await JSON.parse(sessionStorage.getItem('zipCity'));
+  const getZip = data.filter((item) => item.physical_city.toLowerCase() === sessionStorage.getItem('selectedCity').toLowerCase());
   const getCity = data.filter((item) => item.delivery_zipcode === sessionStorage.getItem('zip'));
 
   console.log('zip', getZip);
