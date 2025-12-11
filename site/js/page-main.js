bb362f97b6c4e93d88b1c39c874333bf8b319be8
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-11 13:30:29 -0800

    test searchFunction

--- site/js/page-main.js
+++ site/js/page-main.js
@@ -236,10 +236,10 @@ const searchInFile = async () => {
 
 //   console.log('zip', getZip);
 //   console.log('city', getCity);
-const sets = [new Set(data.map((item) => item.physical_city))];
-console.log(sets);
+  const citySets = [... new Set(data.map((item) => item.physical_city))];
+  return citySets;
 }
-searchInFile();
+console.log(searchInFile());
 
 const listOfCities = ['Seattle', 'Shoreline', 'Bothel', 'Honolulu']
 
