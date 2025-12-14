24f1b431a019e98c7e2957cea2590f5771b8300a
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-13 17:15:57 -0800

    refactoring

--- site/js/app.js
+++ site/js/app.js
@@ -24,6 +24,9 @@ document.addEventListener('DOMContentLoaded', async () => {
             return true;
         };
     };
+
+//listen input field for search engine
+
     
 
 //download list of cities and writing it to session storage
@@ -52,17 +55,20 @@ const inputSearch = (listOfCities) => {
       return [];
     };
      return listOfCities
-      .filter(listOfCities => listOfCities.toLowerCase().startsWith(q));
+      .filter(listOfCities => listOfCities.toLowerCase().startsWith(q))
     };
+      console.log({find});
       return { find };
 };
+
+
+const search = inputSearch(listOfCities);
 
 const finder = () => {
     const search = inputSearch(listOfCities);
 
     cityField.addEventListener('input', (e) => {
-        e.target.value = e.target.value.replace(/[^\p{L}\s-]/gu, '');
-        const inputField = e.target.value;
+        const inputField = e.target.value
         sessionStorage.setItem('input', inputField);
         const result = search.find(inputField);
         return result
