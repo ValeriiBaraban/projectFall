4055f2a762ab36cd9f05545996bc5353d22d9c4f
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-15 11:32:38 -0800

    refactoring

--- site/js/app.js
+++ site/js/app.js
@@ -1,105 +1,100 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    // turn off browser validation
-    document.querySelector('form').setAttribute('novalidate', '');
-    
-    //add animation to Wheater
-    document.querySelector('h1').classList.add('loading-text');
- 
-    //errors for field: City
-    const cityError = document.querySelector('#city-error');
-    const cityField = document.querySelector('#cities');
-    const datalist = document.querySelector('#cities-list');
+document.addEventListener("DOMContentLoaded", async () => {
+  // turn off browser validation
+  document.querySelector("form").setAttribute("novalidate", "");
 
+  //add animation to Wheater
+  document.querySelector("h1").classList.add("loading-text");
 
-//TODO добавить проверку чтоб цифры нельзя было юзать
+  //errors for field: City
+  const cityError = document.querySelector("#city-error");
+  const cityField = document.querySelector("#cities");
+  const datalist = document.querySelector("#cities-list");
 
-    // checking City
-    const checkingFieldName = () => {
-        if(cityField.value.length === 0) {
-            cityError.textContent = "field requared";
-            return false;
-        } else if(cityField.value.length < 3) {
-            cityError.textContent = "min 3 symbols";
-            return false;
-        } else {
-            cityError.textContent = "";
-            return true;
-        };
+  //TODO добавить проверку чтоб цифры нельзя было юзать
+
+  // checking City
+  const checkingFieldName = () => {
+    if (cityField.value.length === 0) {
+      cityError.textContent = "field requared";
+      return false;
+    } else if (cityField.value.length < 3) {
+      cityError.textContent = "min 3 symbols";
+      return false;
+    } else {
+      cityError.textContent = "";
+      return true;
+    }
+  };
+
+  //listen input field for search engine
+
+  //download list of cities and writing it to session storage
+  const getZipCity = async () => {
+    const res = await fetch("/site/delivery_zipcode_physical_city.json");
+
+    const file = await res.json();
+    sessionStorage.setItem("zipCity", JSON.stringify(file));
+    return file;
+  };
+
+  //get list of cities
+  const getCityList = async () => {
+    await getZipCity();
+    const data = await JSON.parse(sessionStorage.getItem("zipCity"));
+    return [...new Set(data.map((item) => item.physical_city))];
+  };
+
+  const listOfCities = await getCityList();
+
+  const inputSearch = (listOfCities) => {
+    const find = (query) => {
+      const q = (query || "").trim().toLowerCase();
+      if (!q) {
+        return [];
+      }
+      return listOfCities.filter((listOfCities) =>
+        listOfCities.toLowerCase().startsWith(q)
+      );
     };
+    return { find };
+  };
 
-//listen input field for search engine
-
-    
-
-//download list of cities and writing it to session storage
-const getZipCity = async () => {
-  const res = await fetch('/site/delivery_zipcode_physical_city.json');
-
-  const file = await res.json();
-  sessionStorage.setItem('zipCity', JSON.stringify(file));
-  return file;
-};
-
-//get list of cities
-const getCityList = async () => {
-  await getZipCity();
-  const data = await JSON.parse(sessionStorage.getItem('zipCity'));
-  return [... new Set(data.map((item) => item.physical_city))];
-}
-
-const listOfCities = await getCityList();
-
-const inputSearch = (listOfCities) => {
-
-  const find = (query) => {
-    const q = (query || '').trim().toLowerCase();
-    if(!q) {
-      return [];
-    };
-     return listOfCities
-      .filter(listOfCities => listOfCities.toLowerCase().startsWith(q))
-    };
-      return { find };
-};
-
-const finder = () => {
+  const finder = () => {
     const search = inputSearch(listOfCities);
 
-    cityField.addEventListener('input', (e) => {
-        const filteredValue = e.target.value.replace(/[^\p{L}\s-]/gu, '');
-        const inputField = filteredValue;
-        sessionStorage.setItem('input', inputField);
-        const result = search.find(inputField);
-          const datalist = document.querySelector('#cities-list');
-                datalist.innerHTML = '';
-
-            result.slice(0, 5).forEach(city => {
-                const option = document.createElement('option');
-                option.value = city;
-                datalist.appendChild(option);
+    cityField.addEventListener("input", (e) => {
+      const filteredValue = e.target.value.replace(/[^\p{L}\s-]/gu, "");
+      const inputField = filteredValue;
+      sessionStorage.setItem("input", inputField);
+      const result = search.find(inputField);
+      const datalist = document.querySelector("#cities-list");
+      datalist.innerHTML = ""; //del old results
+      // Add 5 cities to the inline list
+      result.slice(0, 5).forEach((city) => {
+        const option = document.createElement("option");
+        option.value = city;
+        datalist.appendChild(option);
       });
 
-        console.log('result', result.slice(0, 10));
-        return result
+      console.log("result", result.slice(0, 5));
+      return result;
     });
-};
+  };
 
-  console.log('finder', finder());
+  console.log("finder", finder());
 
-// Validation runs when the button is clicked
-    const submit = document.querySelector('.btn-primary');
-    submit.addEventListener('click', function(e) {
-        e.preventDefault();
-        // Run all validation functions/ consts received true or false
-       const checkingFieldNameBoolean = checkingFieldName();
-       
-       if((checkingFieldNameBoolean)) {
-            sessionStorage.setItem('selectedCity', cityField.value);
-            //sessionStorage.setItem('selectedCountry', country);
-           // sessionStorage.setItem('zip', zip);
-            window.location.href = '/site/page-main.html';
-       };
+  // Validation runs when the button is clicked
+  const submit = document.querySelector(".btn-primary");
+  submit.addEventListener("click", function (e) {
+    e.preventDefault();
+    // Run all validation functions/ consts received true or false
+    const checkingFieldNameBoolean = checkingFieldName();
 
-    });
-
+    if (checkingFieldNameBoolean) {
+      sessionStorage.setItem("selectedCity", cityField.value);
+      //sessionStorage.setItem('selectedCountry', country);
+      // sessionStorage.setItem('zip', zip);
+      window.location.href = "/site/page-main.html";
+    }
+  });
 });
--- site/js/page-main.js
+++ site/js/page-main.js
@@ -5,184 +5,183 @@
 //TODO Sets, updates, or changes local storage
 //TODO Contains form fields, validates those fields
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   // if (!sessionStorage.getItem('allowPage2')) {
   //   window.location.href = 'index.html';
   //   return;
   // }
-//TODO add 12 hours format
-function getTime() {
-  const date = new Date();
-  return [String(date.getHours()).padStart(2, '0'), String(date.getMinutes()).padStart(2, '0')];
-} 
+  //TODO add 12 hours format
+  function getTime() {
+    const date = new Date();
+    return [
+      String(date.getHours()).padStart(2, "0"),
+      String(date.getMinutes()).padStart(2, "0"),
+    ];
+  }
 
-class Geolocation {
-
+  class Geolocation {
     constructor(selectedCity) {
-        this.city = selectedCity;
-        this.geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${this.city}&count=1`;
-        this.BASEURL = 'https://api.open-meteo.com/v1/forecast';
-        this.daily = '&daily=';
-        this.hourly = '&hourly=';
-        this.minutely = '&minutely_15=';
+      this.city = selectedCity;
+      this.geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${this.city}&count=1`;
+      this.BASEURL = "https://api.open-meteo.com/v1/forecast";
+      this.daily = "&daily=";
+      this.hourly = "&hourly=";
+      this.minutely = "&minutely_15=";
     }
 
     async getCoordinates() {
-        const res = await fetch(this.geoUrl);
-        const data = await res.json();
-        const country = data.results[0].country;
-        const latitude = data.results[0].latitude;
-        const longitude = data.results[0].longitude;
-        return [latitude, longitude, country];
-    };
+      const res = await fetch(this.geoUrl);
+      const data = await res.json();
+      const country = data.results[0].country;
+      const latitude = data.results[0].latitude;
+      const longitude = data.results[0].longitude;
+      return [latitude, longitude, country];
+    }
 
     async getWeather() {
-        const [latitude, longitude, country] = await this.getCoordinates();
-        const url = `${this.BASEURL}?latitude=${latitude}&longitude=${longitude}${this.hourly}temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m`;
-        const res = await fetch(url);
-        const data = await res.json();
-        data.country = country;
-        return data;
+      const [latitude, longitude, country] = await this.getCoordinates();
+      const url = `${this.BASEURL}?latitude=${latitude}&longitude=${longitude}${this.hourly}temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m`;
+      const res = await fetch(url);
+      const data = await res.json();
+      data.country = country;
+      return data;
     }
-}
+  }
 
-const normalizeCityName = () => {
-    const selectedCity = sessionStorage.getItem('selectedCity'); //get input str from index.html
-    const normalizedStr = selectedCity.charAt(0).toUpperCase() + selectedCity.slice(1).toLowerCase();
+  const normalizeCityName = () => {
+    const selectedCity = sessionStorage.getItem("selectedCity"); //get input str from index.html
+    const normalizedStr =
+      selectedCity.charAt(0).toUpperCase() +
+      selectedCity.slice(1).toLowerCase();
     return normalizedStr;
-}
-
-const newCity = new Geolocation(normalizeCityName());
-const response = (newCity.getWeather())
-  .then(function(data) {
-    const fetchCurrentWheater = new localStorageForHistory(data, normalizeCityName());
-    
-    fetchCurrentWheater.fetchAll();
-    fetchCurrentWheater.addLocalStorage();
-    
-    
-  });
-  
-  class CurrentWheater {
-  constructor(response, cityName) {
-    this.temperatureCelsius = response.current.temperature_2m;
-    this.temperatureFahrenheit = this.#CToF(this.temperatureCelsius);
-    this.cityName = cityName;
-    this.country = response.country;
-    this.lastUpdated = getTime();
-    this.humidity = response.current.relative_humidity_2m;
-};
-
-  selector = document.querySelector('#current-wheater-container');
-
-  #CToF (celsium) {
-    return (celsium * 9 / 5 + 32).toFixed(1);
   };
 
-  addTime() {
-      const lastUpdated = document.createElement('h4');
+  const newCity = new Geolocation(normalizeCityName());
+  const response = newCity.getWeather().then(function (data) {
+    const fetchCurrentWheater = new localStorageForHistory(
+      data,
+      normalizeCityName()
+    );
+
+    fetchCurrentWheater.fetchAll();
+    fetchCurrentWheater.addLocalStorage();
+  });
+
+  class CurrentWheater {
+    constructor(response, cityName) {
+      this.temperatureCelsius = response.current.temperature_2m;
+      this.temperatureFahrenheit = this.#CToF(this.temperatureCelsius);
+      this.cityName = cityName;
+      this.country = response.country;
+      this.lastUpdated = getTime();
+      this.humidity = response.current.relative_humidity_2m;
+    }
+
+    selector = document.querySelector("#current-wheater-container");
+
+    #CToF(celsium) {
+      return ((celsium * 9) / 5 + 32).toFixed(1);
+    }
+
+    addTime() {
+      const lastUpdated = document.createElement("h4");
       const formattingTime = `${this.lastUpdated[0]} : ${this.lastUpdated[1]}`;
       lastUpdated.textContent = `Last updated at: ${formattingTime}`;
-      lastUpdated.classList = 'updated-time';
+      lastUpdated.classList = "updated-time";
       this.selector.appendChild(lastUpdated);
 
       return lastUpdated;
-  }
-
-  addTemp() {
-    const temp_c = document.createElement('p');
-    const temp_f = document.createElement('p');
-
-    temp_c.textContent = `${this.temperatureCelsius}' °C'`;
-    temp_f.textContent = `${this.temperatureFahrenheit}' °F'`;
-
-
-    this.selector.appendChild(temp_c);
-    this.selector.appendChild(temp_f);
-
-    return [temp_c, temp_f];
-  };
-
-  addLocation() {
-    const country = document.createElement('h2');
-    const city = document.createElement('h3');
-
-    country.classList = 'country';
-    
-    country.textContent = this.country;
-
-    city.classList = 'city';
-    city.textContent = `Current weather in ${this.cityName}:`;
-
-    this.selector.appendChild(country);
-    this.selector.appendChild(city);
-
-    return [country, city];
-};
-
-  addConditions() {
-        const humidity = document.createElement('p');
-        humidity.classList = 'humidity';
-        humidity.textContent = `Humidity ${this.humidity} %`;
-        this.selector.appendChild(humidity);
-        
-        return humidity;
-    };
-    
-    
-    fetchAll() {
-        this.selector.innerHTML = '';
-        this.addLocation();
-        this.addTime();
-        this.addTemp();
-        this.addConditions();
-        
     }
-};
 
-class localStorageForHistory extends CurrentWheater {
-  constructor(response, cityName) {
-    super(response, cityName);
-  };
+    addTemp() {
+      const temp_c = document.createElement("p");
+      const temp_f = document.createElement("p");
 
-  addLocalStorage() {
-    const historicalTime = JSON.parse(localStorage.getItem('WheaterHistory')) || [];
-    historicalTime.push({
-//TODO replace time formatting
-//TODO add autoupdater
-      'city': this.cityName,
-      'time': `${this.lastUpdated[0]}:${this.lastUpdated[1]}`,
-      'C' : this.temperatureCelsius,
-      'F' : this.temperatureFahrenheit,
-    });
+      temp_c.textContent = `${this.temperatureCelsius}' °C'`;
+      temp_f.textContent = `${this.temperatureFahrenheit}' °F'`;
 
-    localStorage.setItem('WheaterHistory', JSON.stringify(historicalTime));
-    console.log('historicalTime:', historicalTime);
-    return historicalTime;
+      this.selector.appendChild(temp_c);
+      this.selector.appendChild(temp_f);
 
+      return [temp_c, temp_f];
+    }
 
-  };
-};
+    addLocation() {
+      const country = document.createElement("h2");
+      const city = document.createElement("h3");
 
-class loadingCard {
-  constructor() {
-      this.card = this.addLoadingScreen();
-      this.selector = document.querySelector('#current-wheater-container');
-  
+      country.classList = "country";
+
+      country.textContent = this.country;
+
+      city.classList = "city";
+      city.textContent = `Current weather in ${this.cityName}:`;
+
+      this.selector.appendChild(country);
+      this.selector.appendChild(city);
+
+      return [country, city];
+    }
+
+    addConditions() {
+      const humidity = document.createElement("p");
+      humidity.classList = "humidity";
+      humidity.textContent = `Humidity ${this.humidity} %`;
+      this.selector.appendChild(humidity);
+
+      return humidity;
+    }
+
+    fetchAll() {
+      this.selector.innerHTML = "";
+      this.addLocation();
+      this.addTime();
+      this.addTemp();
+      this.addConditions();
+    }
   }
 
-  waitingDownload() {
-      this.selector.classList.add('weather-card', 'hidden');
-  };
+  class localStorageForHistory extends CurrentWheater {
+    constructor(response, cityName) {
+      super(response, cityName);
+    }
 
-  addLoadingScreen() {
-      const loadingScreen = document.createElement('div');
-      loadingScreen.classList.add('loading-screen');
-      const spinner = document.createElement('div');
-      spinner.classList.add('spinner');
-      const text = document.createElement('p');
-      text.classList.add('loading-text');
-      text.textContent = 'Loading wheater...';
+    addLocalStorage() {
+      const historicalTime =
+        JSON.parse(localStorage.getItem("WheaterHistory")) || [];
+      historicalTime.push({
+        //TODO replace time formatting
+        //TODO add autoupdater
+        city: this.cityName,
+        time: `${this.lastUpdated[0]}:${this.lastUpdated[1]}`,
+        C: this.temperatureCelsius,
+        F: this.temperatureFahrenheit,
+      });
+
+      localStorage.setItem("WheaterHistory", JSON.stringify(historicalTime));
+      console.log("historicalTime:", historicalTime);
+      return historicalTime;
+    }
+  }
+
+  class loadingCard {
+    constructor() {
+      this.card = this.addLoadingScreen();
+      this.selector = document.querySelector("#current-wheater-container");
+    }
+
+    waitingDownload() {
+      this.selector.classList.add("weather-card", "hidden");
+    }
+
+    addLoadingScreen() {
+      const loadingScreen = document.createElement("div");
+      loadingScreen.classList.add("loading-screen");
+      const spinner = document.createElement("div");
+      spinner.classList.add("spinner");
+      const text = document.createElement("p");
+      text.classList.add("loading-text");
+      text.textContent = "Loading wheater...";
 
       loadingScreen.appendChild(spinner);
       loadingScreen.appendChild(text);
@@ -190,36 +189,29 @@ class loadingCard {
       document.body.appendChild(loadingScreen);
 
       return loadingScreen;
-
-    };
+    }
 
     show() {
-      this.card.classList.remove('hidden');
-      this.selector.classList.add('hidden');
-
-    };
+      this.card.classList.remove("hidden");
+      this.selector.classList.add("hidden");
+    }
 
     hide() {
-      this.card.classList.add('hidden');
-      this.selector.classList.remove('hidden')
-    };
-    
+      this.card.classList.add("hidden");
+      this.selector.classList.remove("hidden");
+    }
+  }
+
+  const loader = new loadingCard();
+
+  //timer for loading wheater
+  const switchLoading = () => {
+    loader.show();
+
+    setTimeout(() => {
+      loader.hide();
+    }, 5000);
   };
 
-
-const loader = new loadingCard();  
-
-//timer for loading wheater
-const switchLoading = () => {
-  loader.show();
-  
-  setTimeout(() => {
-    loader.hide();
-  }, 5000);
-}
-
-switchLoading();
-
-
-
+  switchLoading();
 });
