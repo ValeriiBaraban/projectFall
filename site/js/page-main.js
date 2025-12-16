cf3610c4a3a2b56313b4d3b1917e683dd9170bc1
Ars-R <82769848+Ars-R@users.noreply.github.com>
2025-12-15 16:15:13 -0800

    add ttr message to error

--- site/js/page-main.js
+++ site/js/page-main.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       return normalizedStr;
     } catch (error) {
-      console.error("normalizeCityName error:", error);
+      console.error("normalizeCityName error:", error.message);
     }
   };
 
